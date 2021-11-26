2cd46ef7dda632926a6d0b7f23bed868806d8d69
Aman1727 <amangoyal1727@gmail.com>
2021-11-26 16:36:28 +0530

    Initial Setup + Storyboard Setup

---
+++ Guidebook App/Helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Guidebook App
+//
+//  Created by Aman on 26/11/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var INFO_VIEWCONTROLLER = "InfoVC"
+    static var MAP_VIEWCONTROLLER = "MapVC"
+    static var NOTES_VIEWCONTROLLER = "NotesVC"
+    
+}
---
+++ Guidebook App/ViewControllers/PlaceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceViewController.swift
+//  Guidebook App
+//
+//  Created by Aman on 26/11/21.
+//
+
+import UIKit
+
+class PlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
